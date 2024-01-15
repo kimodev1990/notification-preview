643397bc2c07b34ee4d312691a12efc533871c9c
Kimo Adel <kimodev1990@gmail.com>
2024-01-15 05:00:48 +0200

    files upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+      },
+
+      colors: {
+        "Very-light-grayish-blue": "hsl(210, 60%, 98%)",
+        "Light-grayish-blue-1": "hsl(211, 68%, 94%)",
+        "Light-grayish-blue-2": "hsl(205, 33%, 90%)",
+        "Grayish-blue": "hsl(219, 14%, 63%)",
+        "Dark-grayish-blue": "hsl(219, 12%, 42%)",
+        "Very-dark-blue": "hsl(224, 21%, 14%)",
+      },
+    },
+  },
+  plugins: [],
+};
